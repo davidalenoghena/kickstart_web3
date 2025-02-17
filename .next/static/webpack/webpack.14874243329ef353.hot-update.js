b97aa855890248ec1f67ec87b86873931a0ad74b
David <davidalenoghena@gmail.com>
2025-02-17 19:52:30 +0100

    added props

--- .next/static/webpack/webpack.14874243329ef353.hot-update.js
+++ .next/static/webpack/webpack.14874243329ef353.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "90d4f1a3240c6f77"; }
+/******/ 	__webpack_require__.h = function() { return "4b12234dc99b64c1"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
