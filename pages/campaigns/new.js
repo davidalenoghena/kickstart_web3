fbabd68d89853328cd4db9cb1f3966236599dfcc
David <davidalenoghena@gmail.com>
2025-02-27 21:48:40 +0100

    added an onsubmit event for the form

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
 import { Form, Button, Input, Message } from "semantic-ui-react";
+import web3 from "../../ethereum/web3";
 class CampaignNew extends Component {
     state = {
-        minimumContribution:
+        minimumContribution: ''
+    };
+
+    onSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            // Get the accounts
+            const accounts = await web3.eth.getAccounts();
+            // Create a new campaign
+            await factory.methods.createCampaign(this.state.minimumContribution).send({
+                from: accounts[0]
+            });
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
     };
     render() {
         return (
             <Layout>
                 <h1>New Campaign!</h1>
 
-                <Form>
+                <Form
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input
