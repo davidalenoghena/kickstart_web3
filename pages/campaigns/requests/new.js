0cd4cad26cd1ad319845ec943e428ceff187631e
David <davidalenoghena@gmail.com>
2025-03-18 20:14:35 +0100

    request page

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,8 +1,56 @@
 import React, { Component } from "react";
+import { Button, Form, Input, Message } from "semantic-ui-react";
+import Layout from "../../../components/Layout";
+import Campaign from "../../../ethereum/campaign";
+import web3 from "../../../ethereum/web3";
+import { Link, Router } from "../../../routes";
 
 class RequestNew extends Component {
+    state = {
+        value: "",
+        description: "",
+        recipient: "",
+        loading: false,
+        errorMessage: ""
+    };
+
+    static async getInitialProps(props) {
+        const { address } = props.query;
+        return { address };
+    }
+
     render() {
-        return <h3>Request New</h3>;
+        return (
+            <Layout>
+                <h3>Create a Request</h3>
+                <Form>
+                    <Form.Field>
+                        <label>Description</label>
+                        <Input
+                            value={this.state.description}
+                            onChange={event => this.setState({ description: event.target.value })}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Value in Ether</label>
+                        <Input
+                            value={this.state.value}
+                            onChange={event => this.setState({ value: event.target.value })}
+                            label="ether"
+                            labelPosition="right"
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Recipient</label>
+                        <Input
+                            value={this.state.recipient}
+                            onChange={event => this.setState({ recipient: event.target.value })}
+                        />
+                    </Form.Field>
+                    <Button primary>Create</Button>
+                </Form>
+            </Layout>
+        );
     }
 }
 
